963c00852e11aaf18eaeb57f8beabd593ed6edae
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2024-12-20 23:23:11 +0500

    initial commit

---
+++ homework.py
@@ -0,0 +1,39 @@
+import os, random
+
+os.system("clear")
+
+def random_array(k, head, end):
+    return [random.randint(head, end) for _ in range(k)]
+
+""" Series14 -> K butun soni va nol soni bilan tugovchi butun sonlar ro'yxati berilgan. K sonidan kichik bo'lgan sonlar sonini chiqaruvchi programma tuzilsin. 
+Agar bunday son bo'lmasa 0 chiqarilsin,"""
+def func14(k):
+    pass
+
+
+""" Series15 -> K butun soni va nol soni bilan tugovchi butun sonlar to'plami berilgan. K sonidan katta bo'lgan birinchi son nomerini chiqaruvchi programma tuzilsin.
+Agar bunday son bo'lmasa, nol chiqarilsin."""
+def func15(k):
+    pass
+
+
+""" Series16 -> K butun soni va nol soni bilan tugovchi butun sonlar to'plami berilgan, K sonidan katta bo'lgan oxirgi son nomerini chiqaruvchi programma tuzilsin.
+Agar bunday son bo'lmasa, nol chiqarilsin."""
+def func16(k):
+    pass
+
+
+""" Series17 -> B haqiqiy, n natural soni va n ta haqiqiy son berilgan. Shu sonlarni B soni bilan birga chiqaruvchi programma tuzilsin."""
+def func17(k):
+    pass
+
+
+""" Series18 -> n > 2 natural soni va n ta butun son o'sish tartibida berilgan. Bu sonlarni orasida bir xil qiymatlilari bo'lishi mumkin.
+Har xil qiymatli elementlarni chiqaruvchi programma tuzilsin."""
+def func18(k):
+    pass
+
+
+""" Series19 -> n natural soni va n ta butun son berilgan. Bu sonlar orasidan chap qo'shnisidan kichiklarini va ularning sonini chiqaruvchi programma tuzilsin."""
+def func19(k):
+    pass
---
+++ testing.py
@@ -0,0 +1,19 @@
+import homework as hw
+
+hw.func14(10)
+print()
+
+# hw.func15(10)
+# print()
+
+# hw.func16(10)
+# print()
+
+# hw.func17(10)
+# print()
+
+# hw.func18(10)
+# print()
+
+# hw.func19(10)
+# print()
